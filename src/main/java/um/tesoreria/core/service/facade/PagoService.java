d381192a3265d88dd4d8d98b4aae9b61f0b3f871
dqmdz <daniel.quinterospinto@gmail.com>
2024-11-14 19:46:14 -0300

    Processing Internal Payment

--- src/main/java/um/tesoreria/core/service/facade/PagoService.java
+++ src/main/java/um/tesoreria/core/service/facade/PagoService.java
@@ -1,13 +1,19 @@
 package um.tesoreria.core.service.facade;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import um.tesoreria.core.kotlin.model.ChequeraCuota;
 import um.tesoreria.core.kotlin.model.ChequeraPago;
+import um.tesoreria.core.model.MercadoPagoContext;
 import um.tesoreria.core.service.ChequeraCuotaService;
 import um.tesoreria.core.service.ChequeraPagoService;
 import um.tesoreria.core.service.ChequeraTotalService;
 import um.tesoreria.core.service.MercadoPagoContextService;
 
 @Service
+@Slf4j
 public class PagoService {
 
     private final ChequeraPagoService chequeraPagoService;
@@ -25,13 +31,18 @@ public class PagoService {
     }
 
     public ChequeraPago registraPagoMP(Long mercadoPagoContextId) {
+        log.debug("Processing registraPagoMP");
 
         final Integer MERCADOPAGO = 18;
 
         var mercadoPagoContext = mercadoPagoContextService.findByMercadoPagoContextId(mercadoPagoContextId);
+        logMercadoPagoContext(mercadoPagoContext);
+
         var chequeraCuota = chequeraCuotaService.findByChequeraCuotaId(mercadoPagoContext.getChequeraCuotaId());
+        logChequeraCuota(chequeraCuota);
 
         var nextOrder = chequeraPagoService.nextOrden(chequeraCuota.getFacultadId(), chequeraCuota.getTipoChequeraId(), chequeraCuota.getChequeraSerieId(), chequeraCuota.getProductoId(), chequeraCuota.getAlternativaId(), chequeraCuota.getCuotaId());
+        log.debug("NextOrder = {}", nextOrder);
 
         var chequeraPago = new ChequeraPago.Builder()
                 .chequeraCuotaId(chequeraCuota.getChequeraCuotaId())
@@ -50,7 +61,10 @@ public class PagoService {
                 .tipoPagoId(MERCADOPAGO)
                 .archivo("MercadoPago")
                 .build();
+        logChequeraPago(chequeraPago);
         chequeraPago = chequeraPagoService.add(chequeraPago, chequeraCuotaService);
+        log.debug("After");
+        logChequeraPago(chequeraPago);
 
         marcarPago(chequeraCuota.getFacultadId(), chequeraCuota.getTipoChequeraId(), chequeraCuota.getChequeraSerieId(), chequeraCuota.getProductoId(), chequeraCuota.getAlternativaId(), chequeraCuota.getCuotaId(), chequeraCuotaService);
 
@@ -58,6 +72,7 @@ public class PagoService {
     }
 
     public void marcarPago(Integer facultadId, Integer tipoChequeraId, Long chequeraSerieId, Integer productoId, Integer alternativaId, Integer cuotaId, ChequeraCuotaService chequeraCuotaService) {
+        log.debug("Processing marcarPago");
 
         boolean pagado = chequeraPagoService.isPagado(facultadId, tipoChequeraId, chequeraSerieId, productoId, alternativaId, cuotaId, chequeraCuotaService);
         boolean pagoMarcado = false;
@@ -70,10 +85,50 @@ public class PagoService {
     }
 
     private void calcularPagado(Integer facultadId, Integer tipoChequeraId, Long chequeraSerieId, Integer productoId, Integer alternativaId) {
+        log.debug("Processing calcularPagado");
         var chequeraTotal = chequeraTotalService.findByUnique(facultadId, tipoChequeraId, chequeraSerieId, productoId);
         chequeraTotal.setTotal(chequeraCuotaService.calculateTotalCuotasActivas(facultadId, tipoChequeraId, chequeraSerieId, productoId, alternativaId));
         chequeraTotal.setPagado(chequeraCuotaService.calculateTotalCuotasPagadas(facultadId, tipoChequeraId, chequeraSerieId, productoId, alternativaId));
         chequeraTotal = chequeraTotalService.update(chequeraTotal, chequeraTotal.getChequeraTotalId());
+    }
+
+    private void logMercadoPagoContext(MercadoPagoContext mercadoPagoContext) {
+        log.debug("Processing logMercadoPagoContext");
+        try {
+            log.debug("MercadoPagoContext -> {}", JsonMapper.builder()
+                    .findAndAddModules()
+                    .build()
+                    .writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(mercadoPagoContext));
+        } catch (JsonProcessingException e) {
+            log.error("MercadoPagoContext error {}", e.getMessage());
+        }
+    }
+
+    private void logChequeraCuota(ChequeraCuota chequeraCuota) {
+        log.debug("Processing logChequeraCuota");
+        try {
+            log.debug("ChequeraCuota -> {}", JsonMapper.builder()
+                    .findAndAddModules()
+                    .build()
+                    .writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(chequeraCuota));
+        } catch (JsonProcessingException e) {
+            log.error("ChequeraCuota error {}", e.getMessage());
+        }
+    }
+
+    private void logChequeraPago(ChequeraPago chequeraPago) {
+        log.debug("Processing logChequeraPago");
+        try {
+            log.debug("ChequeraPago -> {}", JsonMapper.builder()
+                    .findAndAddModules()
+                    .build()
+                    .writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(chequeraPago));
+        } catch (JsonProcessingException e) {
+            log.error("ChequeraPago error {}", e.getMessage());
+        }
     }
 
 }
