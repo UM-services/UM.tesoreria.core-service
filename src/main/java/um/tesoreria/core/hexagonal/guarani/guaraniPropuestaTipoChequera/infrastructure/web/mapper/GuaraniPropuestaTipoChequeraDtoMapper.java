5b3cb901c72e8e828283ef37e325406df148fe8c
dqmdz <daniel.quinterospinto@gmail.com>
2026-08-04 20:53:02 -0300

    feat(guarani): enrich proposal type response

--- src/main/java/um/tesoreria/core/hexagonal/guarani/guaraniPropuestaTipoChequera/infrastructure/web/mapper/GuaraniPropuestaTipoChequeraDtoMapper.java
+++ src/main/java/um/tesoreria/core/hexagonal/guarani/guaraniPropuestaTipoChequera/infrastructure/web/mapper/GuaraniPropuestaTipoChequeraDtoMapper.java
@@ -1,12 +1,19 @@
 package um.tesoreria.core.hexagonal.guarani.guaraniPropuestaTipoChequera.infrastructure.web.mapper;
 
 import org.springframework.stereotype.Component;
+import um.tesoreria.core.hexagonal.chequera.tipoChequera.infrastructure.web.mapper.TipoChequeraDtoMapper;
 import um.tesoreria.core.hexagonal.guarani.guaraniPropuestaTipoChequera.domain.model.GuaraniPropuestaTipoChequera;
 import um.tesoreria.core.hexagonal.guarani.guaraniPropuestaTipoChequera.infrastructure.web.dto.GuaraniPropuestaTipoChequeraRequest;
 import um.tesoreria.core.hexagonal.guarani.guaraniPropuestaTipoChequera.infrastructure.web.dto.GuaraniPropuestaTipoChequeraResponse;
 
 @Component
 public class GuaraniPropuestaTipoChequeraDtoMapper {
+    private final TipoChequeraDtoMapper tipoChequeraDtoMapper;
+
+    public GuaraniPropuestaTipoChequeraDtoMapper(TipoChequeraDtoMapper tipoChequeraDtoMapper) {
+        this.tipoChequeraDtoMapper = tipoChequeraDtoMapper;
+    }
+
     public GuaraniPropuestaTipoChequera toDomain(GuaraniPropuestaTipoChequeraRequest request) {
         if (request == null) {
             return null;
@@ -27,6 +34,7 @@ public class GuaraniPropuestaTipoChequeraDtoMapper {
                 .propuestaGuarani(domain.getPropuestaGuarani())
                 .lectivoId(domain.getLectivoId())
                 .tipoChequeraId(domain.getTipoChequeraId())
+                .tipoChequera(tipoChequeraDtoMapper.toResponse(domain.getTipoChequera()))
                 .build();
     }
 }
